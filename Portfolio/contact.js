91cecde5af266f979632cfc16b94fd87450247e4
Pooja Navnath Kale <87610832+kalep679@users.noreply.github.com>
2023-02-07 22:50:50 +0530

    Add files via upload

---
+++ Portfolio/contact.js
@@ -0,0 +1,22 @@
+    <section class="contact-section">
+      <div class="container">
+        <h2 class="text-center mb-4">Contact Me</h2>
+        <form action="submit.php" method="post">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Your Name" name="name">
+          </div>
+          <div class="form-group">
+            <input type="email" class="form-control" placeholder="Your Email" name="email">
+          </div>
+          <div class="form-group">
+            <textarea class="form-control" rows="5" placeholder="Your Message" name="message"></textarea>
+          </div>
+          <input type="submit" value="Submit" class="btn btn-primary btn-block">
+        </form>
+      </div>
+    </section>
+    <footer class="bg-dark text-white p-4 text-center">
+      Copyright &copy; 2023 My Portfolio
+    </footer>
+  </body>
+</html>
